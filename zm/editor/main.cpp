de30a1d789e7f97bda97219666b4c8b5687e2d19
Lautaro Javier Ituarte <lautaro.javier.ituarte@gmail.com>
2016-05-17 00:22:46 -0300

    Cargo un par de imagenes en blanco

--- zm/editor/main.cpp
+++ zm/editor/main.cpp
@@ -13,11 +13,15 @@ int main(int argc, char **argv)
   Gtk::Button * pBtnCrearJugador;
   Gtk::Window * pwindow;
   Gtk::EventBox* pEventBox;
+  Gtk::Image* pImage;
+  Gtk::Image* pImage2;
   builder->get_widget("btnCrearTerreno", pBtnCrearTerreno);
   builder->get_widget("btnCrearEnemigo", pBtnCrearEnemigo);
   builder->get_widget("btnCrearJugador", pBtnCrearJugador);
   builder->get_widget("applicationwindow1", pwindow);
   builder->get_widget("eventbox2", pEventBox);
+  builder->get_widget("image1", pImage);
+  builder->get_widget("image2",pImage2);
 
   pwindow->set_default_size(1024, 768);
 
@@ -41,6 +45,10 @@ int main(int argc, char **argv)
     pEventBox->set_events(Gdk::BUTTON_PRESS_MASK);
     pEventBox->signal_button_press_event().connect (sigc::mem_fun(editor,&Editor::on_eventbox_button_press));
   }
+
+  pImage->set("vacio.jpg");
+
+  pImage2->set("vacio.jpg");
 
   app->run(*pwindow);
 
