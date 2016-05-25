339f40e2156cb12afa9475f6bbe523e3ec257415
Lautaro Javier Ituarte <lautaro.javier.ituarte@gmail.com>
2016-05-25 01:02:26 -0300

    Actualizado serializer para exportar e importar nuevo formato de json

--- zm/json/jsonserializer.h
+++ zm/json/jsonserializer.h
@@ -4,11 +4,20 @@
 #include "json.hpp"
 using json = nlohmann::json;
 
+typedef struct SpawnData
+{
+  int column;
+  int row;
+  int type;
+} Spawn;
+
 typedef struct JsonMap
 {
   std::vector<int> imageNumbers;
   std::vector<std::string> imageNames;
   std::vector<std::string> physics;
+  std::vector<std::string> spawnTypes;
+  std::vector<SpawnData> spawnsData;
 } JsonMap;
 
 class JsonSerializer
