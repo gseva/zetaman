abf24202e30ee0859e0b2518806a716ad1943b03
Seva Gavrilov <gavrilovseva@gmail.com>
2016-05-30 14:13:57 -0300

    Avances, no funciona todavia.

--- zm/client/client.cpp
+++ zm/client/client.cpp
@@ -6,55 +6,107 @@
 
 namespace zm {
 
-Client::Client() {
+
+GameFrame::GameFrame(Client& c) : c_(c), area_(c.serverProxy) {
+  add(area_);
+  c_.serverProxy.connect();
+  c_.serverProxy.startLevel();
+
+  c_.serverProxy.updateHandler.signal_game_update().connect(
+      sigc::mem_fun(area_, &Canvas::updateGameState) );
 }
 
-void Client::run(Glib::RefPtr<Gtk::Application> app) {
-  Window window(this);
-  window.set_default_size(1024, 768);
+GameFrame::~GameFrame() {}
 
-  Canvas area(serverProxy);
-  serverProxy.connect();
-  serverProxy.startLevel();
-  window.add(area);
-  area.show();
-
-  serverProxy.updateHandler.signal_game_update().connect(
-      sigc::mem_fun(area, &Canvas::updateGameState) );
-  app->run(window);
-}
-
-void Client::draw(proto::Game game) {
+void GameFrame::start() {
+  area_.show();
 }
 
 
-Window::Window(Client* c) : c_(c) {
-  // signal_key_press_event().connect(
-  //   sigc::mem_fun(*this, &Window::keyReleased));
+LobbyFrame::LobbyFrame(Client& c) : c_(c), m_button("Start game") {
+  set_border_width(10);
+
+  m_button.signal_clicked().connect(sigc::mem_fun(*this,
+              &LobbyFrame::on_button_clicked));
+
+  add(m_button);
+
+  m_button.show();
+}
+
+LobbyFrame::~LobbyFrame() {}
+
+void LobbyFrame::on_button_clicked() {
+  c_.startGame();
+}
+
+
+GameWindow::GameWindow(Client& c) : c_(c) {
   add_events(Gdk::KEY_PRESS_MASK | Gdk::KEY_RELEASE_MASK);
+
+  GameFrame frame(c_);
+  screens_.add(frame);
+  screens_.set_visible_child(frame);
+  add(screens_);
+  screens_.show();
+
+  set_position(Gtk::WIN_POS_CENTER);
+  set_default_size(1024, 768);
+  show();
+  show_all_children();
 }
 
+GameWindow::~GameWindow() {};
 
-bool Window::on_key_press_event(GdkEventKey* event) {
+bool GameWindow::on_key_press_event(GdkEventKey* event) {
   if (event->keyval == GDK_KEY_space) {
-    c_->serverProxy.jump();
+    c_.serverProxy.jump();
   } else if (event->keyval == GDK_KEY_Right) {
-    c_->serverProxy.moveRight();
+    c_.serverProxy.moveRight();
   } else if (event->keyval == GDK_KEY_Left) {
-    c_->serverProxy.moveLeft();
+    c_.serverProxy.moveLeft();
   }
 
   return Gtk::Window::on_key_press_event(event);
 }
 
-bool Window::on_key_release_event(GdkEventKey* event) {
+bool GameWindow::on_key_release_event(GdkEventKey* event) {
   if (event->keyval == GDK_KEY_Right) {
-    c_->serverProxy.stopHorizontalMove();
+    c_.serverProxy.stopHorizontalMove();
   } else if (event->keyval == GDK_KEY_Left) {
-    c_->serverProxy.stopHorizontalMove();
+    c_.serverProxy.stopHorizontalMove();
   }
 
   return Gtk::Window::on_key_release_event(event);
 }
+
+
+Client::Client() : window_(*this) {
+  // screens_.add(lobby_);
+  // screens_.set_visible_child(lobby_);
+  // screens_.show();
+}
+
+void Client::startLobby() {
+  // lobby_.show();
+}
+
+void Client::startGame() {
+  // GameFrame frame(*this);
+  // frame.add(area_);
+  // area_.show();
+  // frame.start();
+  // frame.show();
+  // lobby_.hide();
+  // screens_.add(frame);
+  // screens_.set_visible_child(frame);
+  // app_->release();
+}
+
+void Client::run(Glib::RefPtr<Gtk::Application> app) {
+  // Gtk::Window window_;
+  app->run(window_);
+}
+
 
 } // zm
--- zm/client/client.h
+++ zm/client/client.h
@@ -4,7 +4,10 @@
 #include <sigc++/sigc++.h>
 
 #include <gtkmm/application.h>
+#include <gtkmm/button.h>
+#include <gtkmm/frame.h>
 #include <gtkmm/drawingarea.h>
+#include <gtkmm/stack.h>
 #include <gtkmm/window.h>
 
 #include "zm/game_protocol.h"
@@ -13,24 +16,62 @@
 
 namespace zm {
 
+class Client;
+
+class LobbyFrame : public Gtk::Frame {
+Client& c_;
+public:
+  explicit LobbyFrame(Client& c);
+  ~LobbyFrame();
+
+protected:
+  void on_button_clicked();
+
+  Gtk::Button m_button;
+};
+
+
+class GameFrame : public Gtk::Frame {
+Client& c_;
+Canvas area_;
+public:
+  explicit GameFrame(Client& c);
+  ~GameFrame();
+
+  void start();
+};
+
+
+class GameWindow : public Gtk::Window {
+public:
+  explicit GameWindow(Client& c);
+  ~GameWindow();
+
+  ServerProxy getProxy();
+
+private:
+  Gtk::Stack screens_;
+  Client& c_;
+
+  bool on_key_press_event(GdkEventKey* event) override;
+  bool on_key_release_event(GdkEventKey* event) override;
+};
+
+
 class Client {
+
+// LobbyFrame lobby_;
+// GameFrame game_;
+Glib::RefPtr<Gtk::Application> app_;
+GameWindow window_;
 public:
   ServerProxy serverProxy;
 
   Client();
   void run(Glib::RefPtr<Gtk::Application> app);
 
-  void draw(proto::Game game);
-};
-
-class Window : public Gtk::Window {
-Client* c_;
-public:
-  explicit Window(Client* c);
-
-private:
-  bool on_key_press_event(GdkEventKey* event) override;
-  bool on_key_release_event(GdkEventKey* event) override;
+  void startLobby();
+  void startGame();
 };
 
 } // zm
